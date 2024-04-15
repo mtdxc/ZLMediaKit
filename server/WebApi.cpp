a37b38d023cc39965dbec94b2a23d59c3e670cd7
cqm <cqm@97kid.com>
2024-04-15 15:54:33 +0800

    Device FFmpegEncoder改进，有FFmpeg的情况下可不依赖x264 diable ENABLE_X264 for VideoStack

--- server/WebApi.cpp
+++ server/WebApi.cpp
@@ -68,7 +68,7 @@
 #include "ZLMVersion.h"
 #endif
 
-#if defined(ENABLE_VIDEOSTACK) && defined(ENABLE_X264) && defined (ENABLE_FFMPEG)
+#if defined(ENABLE_VIDEOSTACK) && defined (ENABLE_FFMPEG)
 #include "VideoStack.h"
 #endif
 
@@ -2399,7 +2399,7 @@ void installWebApi() {
         });
     });
 
-#if defined(ENABLE_VIDEOSTACK) && defined(ENABLE_X264) && defined(ENABLE_FFMPEG)
+#if defined(ENABLE_VIDEOSTACK) && defined(ENABLE_FFMPEG)
     VideoStackManager::Instance().loadBgImg("novideo.yuv");
     NoticeCenter::Instance().addListener(nullptr, Broadcast::kBroadcastStreamNoneReader, [](BroadcastStreamNoneReaderArgs) {
         auto id = sender.getMediaTuple().stream;
