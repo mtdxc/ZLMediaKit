68afc02719f6532522ed0719f9b2039f6ed795df
cqm <cqm@97kid.com>
2025-09-04 08:27:37 +0800

    move srt dir

--- src/Pusher/PusherBase.cpp
+++ src/Pusher/PusherBase.cpp
@@ -13,7 +13,7 @@
 #include "Rtsp/RtspPusher.h"
 #include "Rtmp/RtmpPusher.h"
 #ifdef ENABLE_SRT
-#include "Srt/SrtPusher.h"
+#include "../srt/SrtPusher.h"
 #endif // ENABLE_SRT
 
 using namespace toolkit;
