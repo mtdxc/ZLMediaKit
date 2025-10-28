4bbf515e2bb2cc48be866910ba2e32a897b7b4f7
mtdxc <mtdxc@126.com>
2025-10-29 00:25:20 +0800

    增加SDL3封装类和测试程序: - uvc_server: live uvc camera and microphone stream with rtmp/rtsp url - sdl_player: same as test_player but use new sdl3 api

---
+++ SDL3Test/sdl_player.cpp
@@ -0,0 +1,211 @@
+/*
+ * Copyright (c) 2016-present The ZLMediaKit project authors. All Rights Reserved.
+ *
+ * This file is part of ZLMediaKit(https://github.com/ZLMediaKit/ZLMediaKit).
+ *
+ * Use of this source code is governed by MIT-like license that can be found in the
+ * LICENSE file in the root of the source tree. All contributing project authors
+ * may be found in the AUTHORS file in the root of the source tree.
+ */
+
+#include <signal.h>
+#include "Util/logger.h"
+#include "Common/config.h"
+#include "Player/MediaPlayer.h"
+#include "Codec/Transcode.h"
+#include "SDL3/SDL_Main.h"
+#include "SDLCapture.h"
+//#include <unistd.h>
+
+using namespace std;
+using namespace toolkit;
+using namespace mediakit;
+
+class YuvDisplayer {
+public:
+    using Ptr = std::shared_ptr<YuvDisplayer>;
+
+    YuvDisplayer(void *hwnd = nullptr, const char *title = "untitled") {
+        _title = title;
+        _hwnd = hwnd;
+    }
+
+    virtual ~YuvDisplayer() {
+        if (_texture) {
+            SDL_DestroyTexture(_texture);
+            _texture = nullptr;
+        }
+        if (_render) {
+            SDL_DestroyRenderer(_render);
+            _render = nullptr;
+        }
+        if (_win) {
+            SDL_DestroyWindow(_win);
+            _win = nullptr;
+        }
+    }
+
+    bool displayYUV(AVFrame *pFrame) {
+        if (!_win) {
+            if (_hwnd) {
+                SDL_PropertiesID props = SDL_CreateProperties();
+                // 根据平台设置正确的属性
+                const char *platform = SDL_GetPlatform();
+
+                if (strcmp(platform, "Windows") == 0) {
+                    SDL_SetPointerProperty(props, SDL_PROP_WINDOW_CREATE_WIN32_HWND_POINTER, _hwnd);
+                } else if (strcmp(platform, "Linux") == 0) {
+                    // 尝试 X11
+                    // SDL_SetPointerProperty(props, SDL_PROP_WINDOW_CREATE_X11_WINDOW_POINTER, _hwnd);
+                } else if (strcmp(platform, "macOS") == 0) {
+                    SDL_SetPointerProperty(props, SDL_PROP_WINDOW_CREATE_COCOA_WINDOW_POINTER, _hwnd);
+                }
+                _win = SDL_CreateWindowWithProperties(props);
+                SDL_DestroyProperties(props);
+            } else {
+                _win = SDL_CreateWindow(_title.data(), pFrame->width, pFrame->height, SDL_WINDOW_OPENGL);
+            }
+        }
+        if (_win && !_render) {
+            _render = SDL_CreateRenderer(_win, "direct3d,opengl,software"); // "direct3d", "metal", "software", "opengl"
+        }
+        if (_render && (!_texture|| _texture->w != pFrame->width || _texture->h != pFrame->height)) {
+            if (_texture) {
+                SDL_DestroyTexture(_texture);
+                _texture = nullptr;
+            }
+            if (pFrame->format == AV_PIX_FMT_NV12) {
+                _texture = SDL_CreateTexture(_render, SDL_PIXELFORMAT_NV12, SDL_TEXTUREACCESS_STREAMING, pFrame->width, pFrame->height);
+            } else {
+                _texture = SDL_CreateTexture(_render, SDL_PIXELFORMAT_IYUV, SDL_TEXTUREACCESS_STREAMING, pFrame->width, pFrame->height);
+            }
+        }
+        if (_texture) {
+            if (pFrame->format == AV_PIX_FMT_NV12) {
+                SDL_UpdateNVTexture(_texture, nullptr, 
+                    pFrame->data[0], pFrame->linesize[0], 
+                    pFrame->data[1], pFrame->linesize[1]);
+            } else {
+                SDL_UpdateYUVTexture(_texture, nullptr, 
+                    pFrame->data[0], pFrame->linesize[0], 
+                    pFrame->data[1], pFrame->linesize[1], 
+                    pFrame->data[2], pFrame->linesize[2]);
+            }
+
+            // SDL_UpdateTexture(_texture, nullptr, pFrame->data[0], pFrame->linesize[0]);
+            SDL_RenderClear(_render);
+            SDL_RenderTexture(_render, _texture, nullptr, nullptr);
+            SDL_RenderPresent(_render);
+            return true;
+        }
+        return false;
+    }
+
+private:
+    std::string _title;
+    void *_hwnd = nullptr;
+    SDL_Window *_win = nullptr;
+    SDL_Renderer *_render = nullptr;
+    SDL_Texture *_texture = nullptr;
+};
+
+
+SDLCapture loop;
+int main(int argc, char *argv[]) {
+#ifdef _WIN32
+    // 1. 首先调用AllocConsole创建一个控制台窗口
+    AllocConsole();
+
+    // 2. 但此时调用cout或者printf都不能正常输出文字到窗口（包括输入流cin和scanf）, 所以需要如下重定向输入输出流：
+    FILE *stream;
+    freopen_s(&stream, "CON", "r", stdin); //重定向输入流
+    freopen_s(&stream, "CON", "w", stdout); //重定向输入流
+
+    // 清除流缓冲区, 在win11上还是无法输出文字，需要在加入如下代码
+    std::cin.clear();
+    std::cout.clear();
+
+    // 3. 如果我们需要用到控制台窗口句柄，可以调用FindWindow取得：
+    SetConsoleTitleA(argv[0]); //设置窗口名
+#endif 
+
+    // 设置退出信号处理函数
+    signal(SIGINT, [](int) { loop.shutdown(); });
+    // 设置日志
+    Logger::Instance().add(std::make_shared<ConsoleChannel>());
+    Logger::Instance().setWriter(std::make_shared<AsyncLogWriter>());
+
+    if (argc < 2) {
+        std::cout << "usage:" << argv[0] << "rtxp_url[rtp_type][play_track]\r\n example:"
+                << argv[0] << " rtsp://admin:123456@127.0.0.1/live/0 0\r\n";
+        return 0;
+    }
+
+    char *url = argv[1];
+    auto player = std::make_shared<MediaPlayer>();
+    // sdl要求在main线程初始化
+    auto displayer = std::make_shared<YuvDisplayer>(nullptr, url);
+    weak_ptr<MediaPlayer> weakPlayer = player;
+    player->setOnPlayResult([weakPlayer, displayer](const SockException &ex) {
+        InfoL << "OnPlayResult:" << ex.what();
+        auto strongPlayer = weakPlayer.lock();
+        if (ex || !strongPlayer) {
+            return;
+        }
+
+        auto videoTrack = dynamic_pointer_cast<VideoTrack>(strongPlayer->getTrack(TrackVideo, false));
+        auto audioTrack = dynamic_pointer_cast<AudioTrack>(strongPlayer->getTrack(TrackAudio, false));
+
+        if (videoTrack) {
+            videoTrack->addRawDelegate([displayer](const FFmpegFrame::Ptr &yuv) {
+                loop.doTask([yuv, displayer]() {
+                    // sdl要求在main线程渲染
+                    displayer->displayYUV(yuv.get());
+                    return true;
+                });
+            });
+        }
+
+        if (audioTrack) {
+            //FFmpeg解码时已经统一转换为16位整型pcm
+            auto pcms = std::make_shared<PcmBuffer<short>>();
+            loop.startAudioPlay(audioTrack->getAudioSampleRate(), audioTrack->getAudioChannel());
+            loop.setPcmFillCallback([pcms](short* pcm, int samples, int channel) { 
+                pcms->Read(pcm, samples * channel);
+            });
+            FFmpegSwr::Ptr swr;
+            audioTrack->addRawDelegate(
+                [swr, pcms](const FFmpegFrame::Ptr &frame) mutable {
+                if (!swr) {
+#if LIBAVCODEC_VERSION_INT >= FF_CODEC_VER_7_1
+                    swr = std::make_shared<FFmpegSwr>(AV_SAMPLE_FMT_S16, &(frame->ch_layout), frame->sample_rate);
+#else
+                    swr = std::make_shared<FFmpegSwr>(AV_SAMPLE_FMT_S16, frame->channels, frame->channel_layout, frame->sample_rate);
+#endif
+                }
+                auto pcm = swr->inputFrame(frame);
+                auto len = pcm->nb_samples * FFmpegFrame::getChannels(pcm);// * av_get_bytes_per_sample((enum AVSampleFormat)pcm->format);
+                pcms->Write((short*)pcm->data[0], len);
+                //audio_player->playPCM((const char *)(pcm->data[0]), MIN(len, frame->linesize[0]));
+            });
+        }
+    });
+
+    player->setOnShutdown([](const SockException &ex) { WarnL << "play shutdown: " << ex.what(); });
+
+    // 不等待track ready再回调播放成功事件，这样可以加快秒开速度
+    (*player)[Client::kWaitTrackReady] = false;
+    if (argc > 2) {
+        (*player)[Client::kRtpType] = atoi(argv[2]);
+    }
+    if (argc > 3) {
+        (*player)[Client::kPlayTrack] = atoi(argv[3]);
+    }
+    player->play(url);
+
+    loop.runLoop();
+
+    sleep(1);
+    return 0;
+}
+
---
+++ SDL3Test/uvc_server.cpp
@@ -0,0 +1,212 @@
+﻿/*
+ * Copyright (c) 2016-present The ZLMediaKit project authors. All Rights Reserved.
+ *
+ * This file is part of ZLMediaKit(https://github.com/ZLMediaKit/ZLMediaKit).
+ *
+ * Use of this source code is governed by MIT-like license that can be found in the
+ * LICENSE file in the root of the source tree. All contributing project authors
+ * may be found in the AUTHORS file in the root of the source tree.
+ */
+
+#include <iostream>
+
+#include "Util/logger.h"
+#include "Util/SSLBox.h"
+#include "Util/onceToken.h"
+#include "Network/TcpServer.h"
+#include "Poller/EventPoller.h"
+
+#include "Common/config.h"
+#include "Rtsp/RtspSession.h"
+#include "Rtmp/RtmpSession.h"
+#include "Http/WebSocketSession.h"
+
+#include "SDLCapture.h"
+#include "Common/Device.h"
+
+using namespace std;
+using namespace toolkit;
+using namespace mediakit;
+
+namespace mediakit {
+// //////////HTTP配置///////////
+#define THEAD_COUNT "general.threads"
+namespace Camera {
+#define CAMERA_FIELD "camera."
+const string kWidth = CAMERA_FIELD"width";
+const string kHeight = CAMERA_FIELD"height";
+const string kFramerate = CAMERA_FIELD"framerate";
+const string kBitrate = CAMERA_FIELD"bitrate";
+const string kDevice = CAMERA_FIELD"device";
+onceToken token1([](){
+    mINI::Instance()[kDevice] = 0;
+    mINI::Instance()[kWidth] = 800;
+    mINI::Instance()[kHeight] = 600;
+    mINI::Instance()[kFramerate] = 30;
+    mINI::Instance()[kBitrate] = 500000;
+}, nullptr);
+} // namespace Camera 
+namespace Microphone {
+#define MIC_FIELD "mic."
+const string kSamplerate = MIC_FIELD"samplerate";
+const string kChannel = MIC_FIELD"channels";
+const string kBitrate = MIC_FIELD"bitrate";
+const string kDevice = MIC_FIELD"device";
+onceToken token1([](){
+    mINI::Instance()[kDevice] = 0;
+    mINI::Instance()[kSamplerate] = 44100;
+    mINI::Instance()[kChannel] = 1;
+    mINI::Instance()[kBitrate] = 64000;
+}, nullptr);
+} // namespace Microphone 
+
+namespace Http {
+#define HTTP_FIELD "http."
+#define HTTP_PORT 80
+const string kPort = HTTP_FIELD"port";
+#define HTTPS_PORT 443
+const string kSSLPort = HTTP_FIELD"sslport";
+onceToken token1([](){
+    mINI::Instance()[kPort] = HTTP_PORT;
+    mINI::Instance()[kSSLPort] = HTTPS_PORT;
+    mINI::Instance()[THEAD_COUNT] = std::thread::hardware_concurrency();
+},nullptr);
+}//namespace Http
+
+// //////////RTSP服务器配置///////////
+namespace Rtsp {
+#define RTSP_FIELD "rtsp."
+#define RTSP_PORT 554
+#define RTSPS_PORT 322
+const string kPort = RTSP_FIELD"port";
+const string kSSLPort = RTSP_FIELD"sslport";
+onceToken token1([](){
+    mINI::Instance()[kPort] = RTSP_PORT;
+    mINI::Instance()[kSSLPort] = RTSPS_PORT;
+},nullptr);
+
+} //namespace Rtsp
+
+// //////////RTMP服务器配置///////////
+namespace Rtmp {
+#define RTMP_FIELD "rtmp."
+#define RTMP_PORT 1935
+const string kPort = RTMP_FIELD"port";
+onceToken token1([](){
+    mINI::Instance()[kPort] = RTMP_PORT;
+},nullptr);
+} //namespace RTMP
+}  // namespace mediakit
+
+
+int main(int argc,char *argv[]) {
+
+    Logger::Instance().add(std::make_shared<ConsoleChannel>());
+    Logger::Instance().add(std::make_shared<FileChannel>());
+    Logger::Instance().setWriter(std::make_shared<AsyncLogWriter>());
+
+    SDLCapture cap;
+    std::vector<Device> devs, adevs;
+    cap.getAudioCaputreDevice(adevs);
+    cap.getVideoCaptureDevice(devs);
+    if (devs.empty() && adevs.empty()) {
+        ErrorL << "no device found!";
+        return -1;
+    }
+    InfoL << adevs.size() << " microphone devices:";
+    for (auto& dev: adevs) {
+        InfoL << dev.id << "> " << dev.name;
+    }
+    InfoL << devs.size() << " camera devices:";
+    for (auto& dev: devs) {
+        InfoL << dev.id << "> " << dev.name;
+    }
+
+    loadIniConfig();
+
+    size_t threads = mINI::Instance()[THEAD_COUNT];
+    EventPollerPool::setPoolSize(threads);
+    WorkThreadPool::setPoolSize(threads);
+    // EventPollerPool::enableCpuAffinity(affinity);
+
+    // 加载证书，证书包含公钥和私钥
+    SSL_Initor::Instance().loadCertificate((exeDir() + "ssl.p12").data());
+    // 信任某个自签名证书
+    SSL_Initor::Instance().trustCertificate((exeDir() + "ssl.p12").data());
+    // 不忽略无效证书证书(例如自签名或过期证书)
+    SSL_Initor::Instance().ignoreInvalidCertificate(false);
+
+    std::string listen_ip = mINI::Instance()[General::kListenIP];
+    uint16_t rtspPort = mINI::Instance()[Rtsp::kPort];
+    uint16_t rtspsPort = mINI::Instance()[Rtsp::kSSLPort];
+    uint16_t rtmpPort = mINI::Instance()[Rtmp::kPort];
+    uint16_t httpPort = mINI::Instance()[Http::kPort];
+    uint16_t httpsPort = mINI::Instance()[Http::kSSLPort];
+
+    TcpServer::Ptr rtspSrv(new TcpServer());
+    TcpServer::Ptr rtmpSrv(new TcpServer());
+    TcpServer::Ptr httpSrv(new TcpServer());
+    TcpServer::Ptr httpsSrv(new TcpServer());
+
+    rtspSrv->start<RtspSession>(rtspPort, listen_ip);//默认554
+    rtmpSrv->start<RtmpSession>(rtmpPort, listen_ip);//默认1935
+    httpSrv->start<HttpSession>(httpPort, listen_ip);//默认80
+    httpsSrv->start<HttpsSession>(httpsPort, listen_ip);//默认443
+
+    // 支持ssl加密的rtsp服务器，可用于诸如亚马逊echo show这样的设备访问
+    TcpServer::Ptr rtspSSLSrv(new TcpServer());
+    rtspSSLSrv->start<RtspSessionWithSSL>(rtspsPort, listen_ip);//默认322
+
+    MediaTuple tuple(DEFAULT_VHOST, "live", "camera");
+    auto dev = std::make_shared<DevChannel>(tuple);
+    uint64_t startTick = SDL_GetTicks();
+
+    GET_CONFIG(uint32_t, device, Microphone::kDevice);
+    GET_CONFIG(int, samplerate, Microphone::kSamplerate);
+    GET_CONFIG(int, channel, Microphone::kChannel);
+    
+    if (cap.startAudioRecord(samplerate,channel)) {
+        AudioInfo ainfo;
+        ainfo.iSampleBit = 16;
+        auto& aspec = cap.getRecordSpec();
+        ainfo.iSampleRate = aspec.freq;
+        ainfo.iChannel = aspec.channels;
+        ainfo.codecId = CodecAAC;
+        dev->initAudio(ainfo);
+        cap.setPcmCallback([dev, startTick](short* pcm, int samples, int channel) {
+            dev->inputPCM((char *)pcm, samples * 2 * channel, SDL_GetTicks() - startTick);
+        });
+    }
+
+    GET_CONFIG(uint32_t, vbitrate, Camera::kBitrate);
+    GET_CONFIG(uint32_t, width, Camera::kWidth);
+    GET_CONFIG(uint32_t, height, Camera::kHeight);
+    GET_CONFIG(uint32_t, framerate, Camera::kFramerate);
+    if (devs.size() && cap.startCamera(width, height, framerate, devs[0].id)) {
+        VideoInfo vinfo;
+        auto& vspec = cap.getVideoSpec();
+        vinfo.iWidth = vspec.width;
+        vinfo.iHeight = vspec.height;
+        vinfo.iFrameRate = vspec.framerate_numerator / vspec.framerate_denominator;
+        vinfo.codecId = CodecH264;
+        vinfo.iBitRate = vbitrate;
+        dev->initVideo(vinfo);
+
+        cap.setYuvCallback([dev, startTick](uint8_t* yuv, int width, int height) {
+            char *plan[3];
+            int linesize[3];
+            int size = width * height;
+            plan[0] = (char *)yuv;
+            plan[1] = plan[0] + size;
+            plan[2] = plan[1] + size / 4;
+            linesize[0] = width;
+            linesize[1] = width / 2;
+            linesize[2] = width / 2;
+            dev->inputYUV(plan, linesize, SDL_GetTicks() - startTick);
+        });
+    }
+
+    getchar();
+    return 0;
+}
+
